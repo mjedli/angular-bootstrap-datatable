491b2306944774260aef439a111684aca695fa43
mjedli <jedli.mejdi.recup@gmail.com>
2020-06-19 22:14:07 +0200

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { AppServiceService } from './app-service.service';
+import { AppService } from './app.service';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ],
-  providers: [AppServiceService]
+  providers: [AppService]
 })
 export class AppModule { }
