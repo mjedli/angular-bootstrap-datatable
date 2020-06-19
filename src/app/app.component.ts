ba644dda70e7415c78fab3bcd5271dcf051111f8
mjedli <jedli.mejdi.recup@gmail.com>
2020-06-19 22:04:35 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,10 +12,10 @@ export class AppComponent  {
 
   myComponent  : MyComponent[] = [
     {name : "Angelica", position : "Ramos", office : "Chief Executive Officer (CEO)", startDate : "London", salary: "$1,200,000"},
-    {name : "Angelica", position : "Ramos", office : "Chief Executive Officer (CEO)", startDate : "London", salary: "$1,200,000"},
-    {name : "Angelica", position : "Ramos", office : "Chief Executive Officer (CEO)", startDate : "London", salary: "$1,200,000"},
-    {name : "Angelica", position : "Ramos", office : "Chief Executive Officer (CEO)", startDate : "London", salary: "$1,200,000"},
-    {name : "Angelica", position : "Ramos", office : "Chief Executive Officer (CEO)", startDate : "London", salary: "$1,200,000"},
+    {name : "Bradley", position : "Ramos", office : "Chief Executive Officer (CEO)", startDate : "Paris", salary: "$1,200,000"},
+    {name : "Brielle", position : "Ramos", office : "Chief Executive Officer (CEO)", startDate : "Tokyo", salary: "$1,200,000"},
+    {name : "Bruno", position : "Ramos", office : "Chief Executive Officer (CEO)", startDate : "San Francisco", salary: "$1,200,000"},
+    {name : "Caesar", position : "Ramos", office : "Chief Executive Officer (CEO)", startDate : "New York", salary: "$1,200,000"},
   ];
 
 
